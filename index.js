da51957b81d22fd2e599f74f5de5cb408e2f273f
Patrick Abejar <wireless.patrick@gmail.com>
2018-11-14 14:10:19 -0500

    added game toolbar

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const lozApp = angular.module('lozApp', ['ngRoute'])
 
-lozApp.service('game', function() {
-  this.setCurrentGame = gameObj => this.currentGame = gameObj
-  this.getCurrentGame = () => this.currentGame
+lozApp.service('allGames', function($http) {
+  this.getAllGames = () => $http.get('http://localhost:3000/games')
 })
 
-lozApp.controller('gameController', ['$scope', '$routeParams', '$http', 'game', function($scope, $routeParams, $http, game) {
+lozApp.controller('gameController', ['$scope', '$routeParams', '$http', '$location', '$route', '$rootScope', 'allGames',
+function($scope, $routeParams, $http, $location, $route, $rootScope, allGames) {
+  console.log($route, $rootScope)
+  $scope.changeGame = (id) => {
+    $scope.currentGame = $scope.allGames.find(game => game.id === id)
 
-  $http.get('http://localhost:3000/games')
-  .then(data => data.data.find(game => game.id === parseInt($routeParams.id)))
-  .then(gameObj => {
-    game.setCurrentGame(gameObj)
-    $scope.currentGame = game.getCurrentGame()
+    // START - There is no native way in AngularJS to change URL without reload
+    // Source: https://www.consolelog.io/angularjs-change-path-without-reloading/
+    const original = $location.path
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            const lastRoute = $route.current
+            const un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute
+                un()
+            })
+        }
+        return original.apply($location, [path])
+    }
+    $location.path(`/games/${id}`, false)
+    $location.replace()
+    // END - There is no native way in AngularJS to change URL without reload
+
+  }
+  allGames.getAllGames().then(data => {
+    $scope.allGames = data.data
+    $scope.currentGame = $scope.allGames.find(game => game.id === parseInt($routeParams.id))
   })
-}])
-
-lozApp.controller('currentGameController', ['$scope', 'game', function($scope, game) {
-  $scope.currentGame = game.getCurrentGame()
 }])
 
 lozApp.config(function($routeProvider) {
@@ -28,9 +43,4 @@ lozApp.config(function($routeProvider) {
     templateUrl: './templates/gameDetail.html',
     controller: 'gameController'
   })
-  .when('/currentGame', {
-    templateUrl: './templates/currentGame.html',
-    controller: 'currentGameController'
-  })
-
 })
